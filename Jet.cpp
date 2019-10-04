f416b5e9647741c0a5ff286eb9221af6d091265b
Sean Bemiller <yuhsredux@gmail.com>
2019-10-04 16:34:32 -0400

    Added Jet and Skateboard files
    Added and completed Jet class, which inherits from
    PoweredVehical class.
    Added Skateboard class fils but have not yet implemented
    them.

---
+++ Jet.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Sean Bemiller on 10/4/19.
+//
+
+#include "Jet.h"
+#include <cstdlib>
+
+Jet::Jet() {
+    numberOfEngines = 1;
+    setBrand("Custom");
+    setModel("Custom");
+}
+
+Jet::Jet(string brand, string model, string fuelType, int numbOfEngines) {
+    setBrand(brand);
+    setModel(model);
+    setFuelType(fuelType);
+    setNumOfEngines(numbOfEngines);
+}
+
+Jet::~Jet() = default;
+
+int Jet::getNumOfEngines() {
+    return numberOfEngines;
+}
+
+void Jet::setNumOfEngines(int numOfEngines) {
+    if (numOfEngines >= 1) {
+        numberOfEngines = numOfEngines;
+    } else {
+        numberOfEngines = 1;
+    }
+
+}
+
+double Jet::mileageEstimate(double time) {
+    double mileage = ((rand() % 61) + 40 ) * time;
+    if (numberOfEngines > 2) {
+        mileage = mileage * 1.055(numberOfEngines);
+    }
+    return mileage;
+}
+
+string Jet::toString() {
+    return "-> Jet\n" + PoweredVehicle::toString() + "\n\tNumber of Engines: " +
+           getNumOfEngines();
+}
---
+++ Jet.h
@@ -0,0 +1,29 @@
+//
+// Created by Sean Bemiller on 10/4/19.
+//
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+    int numberOfEngines;
+
+public:
+    Jet();
+
+    explicit Jet(string brand, string model, string fuelType,
+                 int numberOfEngines);
+
+    virtual ~Jet();
+    int getNumOfEngines();
+    void setNumOfEngines(int numberOfEngines);
+    virtual double mileageEstimate(double time);
+	virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
